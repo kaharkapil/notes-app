60d0b9f0ed7b771dec01249a662c7ab329e129ba
Kapil Kahar <52687372+kaharkapil@users.noreply.github.com>
2024-08-05 19:10:51 +0530

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,10 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import notesReducer from './notesSlice';
 
 const store = configureStore({
-reducer: {
-notes: notesReducer,
-},
+  reducer: {
+    notes: notesReducer,
+  },
 });
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export default store;
